68134753f560cf71dc19edaaa3172ee355b0fb22
Yash Garg <yashgarg5101@gmail.com>
2021-05-26 20:32:35 +0530

    added navbar component

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import Row from "./components/Row";
 import requests from "./requests";
 import Banner from "./components/Banner";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="app">
       {/* navbar */}
+      <Nav />
       {/* banner */}
       <Banner />
       <Row
---
+++ netflix/src/components/Nav.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+        alt="Netflix Logo"
+        className="nav_logo"
+      />
+      <img
+        className="nav_avatar"
+        src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+        alt="Profile Avatar"
+      />
+    </div>
+  );
+}
+
+export default Nav;
